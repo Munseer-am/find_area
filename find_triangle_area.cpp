b8327c71603dfbdb07af628a07d6af440563ce5e
Munseer-am <munnamunseer7@gmail.com>
2022-11-04 21:04:44 +0530

    Initial commit

--- find_triangle_area.cpp
+++ find_triangle_area.cpp
@@ -6,7 +6,7 @@ int main() {
     int a, b, c, s, area;
     cout << "Enter sides of triangle: ";
     cin >> a >> b >> c;
-    if (a+b > c) {
+    if ((a+b > c) && (a+c > b) && (c+b > a)) {
         s = (a + b + c) / 2;
         area = sqrt(s*(s-a)*(s-b)*(s-c));
         cout << area;
